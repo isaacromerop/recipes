67f921bd7cf23bd36af43e7daf52fa8e3f676877
Isaac Romero <isaacmromero@gmail.com>
2021-02-16 10:20:58 -0500

    Navbar 50% done

--- client-recipes/components/NavBar.jsx
+++ client-recipes/components/NavBar.jsx
@@ -1,33 +1,34 @@
 import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
-import { appearUp } from "../styles/animations";
 
 const NavBar = () => {
   return (
     <motion.nav
       initial={{ y: -100 }}
       animate={{ y: 0 }}
-      transition={{ delay: 0.5, duration: 0.5, type: "tween" }}
+      transition={{ delay: 0.7, duration: 0.5, type: "tween" }}
       className="navbar"
     >
-      <div className="logo">
-        <span style={{color: "white"}}>Logo Navbar</span>
-      </div>
-      <div className="navigation">
-        <Link href="/login">
+      <div className="nav-container">
+        <div className="logo">
+          <span>Mr. Cook</span>
+        </div>
+        <div className="navigation">
+          <Link href="/login">
+            <a>
+              <button>Login</button>
+            </a>
+          </Link>
+          <Link href="/profile">
+            <a>
+              <button>Profile</button>
+            </a>
+          </Link>
           <a>
-            <button>Login</button>
+            <button>Logout</button>
           </a>
-        </Link>
-        <Link href="/profile">
-          <a>
-            <button>Profile</button>
-          </a>
-        </Link>
-        <a>
-          <button>Logout</button>
-        </a>
+        </div>
       </div>
     </motion.nav>
   );
