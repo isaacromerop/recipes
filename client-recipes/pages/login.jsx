4837559fa704b3aeef1e732cc6d24a2b525c4b30
Isaac Romero <isaacmromero@gmail.com>
2021-02-18 10:33:11 -0500

    Working on login page

--- client-recipes/pages/login.jsx
+++ client-recipes/pages/login.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { scaleUp, showUp } from "../styles/animations";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import Link from "next/link";
+import { useMutation, gql } from "@apollo/client";
+
+const USER_AUTH = gql`
+  mutation userAuth($input: AuthInput) {
+    userAuth(input: $input) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
+  const [userAuth] = useMutation(USER_AUTH);
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
     validationSchema: Yup.object({
-      email: Yup.string().required("Please, enter your email."),
+      email: Yup.string()
+        .required("Please, enter your email.")
+        .email("Please, enter a valid email."),
       password: Yup.string().required("Please, enter your password."),
     }),
     onSubmit: async (values) => {
@@ -27,28 +40,84 @@ const Login = () => {
     >
       <div className="login-content">
         <div className="login-form">
-          <h1>Log In</h1>
+          <motion.h1
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+          >
+            Log In
+          </motion.h1>
           <motion.form variants={scaleUp} onSubmit={formik.handleSubmit}>
-            <div className="login-email">
+            <motion.div className="login-email">
               <label htmlFor="email">Email:</label>
               <input
-                name="email"
+                id="email"
                 type="email"
                 value={formik.values.email}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
-            </div>
-            <div className="login-email">
+              <AnimatePresence>
+                {formik.touched.email && formik.errors.email && (
+                  <motion.div
+                    initial={{ x: -10 }}
+                    animate={{ x: 0 }}
+                    transition={{
+                      duration: 0.5,
+                      type: "spring",
+                      stiffness: 500,
+                    }}
+                    exit={{ opacity: 0, transition: { type: "tween" } }}
+                    className="error-message"
+                  >
+                    <p>{formik.errors.email}</p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.div>
+
+            <motion.div className="login-password">
               <label htmlFor="password">Password:</label>
               <input
-                name="password"
+                id="password"
                 type="password"
                 value={formik.values.password}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
-            </div>
+              <AnimatePresence>
+                {formik.touched.password && formik.errors.password && (
+                  <motion.div
+                    initial={{ x: -10 }}
+                    animate={{ x: 0 }}
+                    transition={{
+                      duration: 0.5,
+                      type: "spring",
+                      stiffness: 500,
+                    }}
+                    exit={{ opacity: 0, transition: { type: "tween" } }}
+                    className="error-message"
+                  >
+                    <p>{formik.errors.password}</p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.div>
+            <button className="login-button" type="submit">
+              LogIn
+            </button>
           </motion.form>
         </div>
+        <motion.p
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1, duration: 0.5, type: "tween" }}
+        >
+          Don't have an account? Register{" "}
+          <Link href="/register">
+            <a>here</a>
+          </Link>
+        </motion.p>
       </div>
     </motion.div>
   );
