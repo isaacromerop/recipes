2ac89761e8382d2966e034cb963197ba5af8a4a4
Isaac Romero <isaacmromero@gmail.com>
2021-03-04 13:09:59 -0500

    Transition for RecipePreview added

--- client-recipes/components/RecipePreview.jsx
+++ client-recipes/components/RecipePreview.jsx
@@ -5,7 +5,7 @@ import SaveButton from "./SaveButton";
 import RemoveButton from "./RemoveButton";
 import useUserStore from "../context/userContext";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { scaleUp } from "../styles/animations";
 
 const RecipePreview = ({ id, title, img, _id }) => {
@@ -18,31 +18,29 @@ const RecipePreview = ({ id, title, img, _id }) => {
     });
   };
   return (
-    <AnimatePresence>
-      <motion.div
-        variants={scaleUp}
-        initial="hidden"
-        animate="visible"
-        className="ui fluid card card-container"
-      >
-        <Image size="medium" src={img} width={312} height={231} />
-        <Card.Content className="card-content">
-          <div onClick={() => recipeDetails(id)} className="card-header">
-            {title}
-          </div>
-          <Card.Meta className="card-meta">
-            <span className="date">id: {id}</span>
-            {user &&
-              (router.pathname === "/bycuisine" ||
-              router.pathname === "/bydish" ? (
-                <SaveButton id={id} title={title} img={img} />
-              ) : (
-                <RemoveButton id={_id} />
-              ))}
-          </Card.Meta>
-        </Card.Content>
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      variants={scaleUp}
+      initial="hidden"
+      animate="visible"
+      className="ui fluid card card-container"
+    >
+      <Image size="medium" src={img} width={312} height={231} />
+      <Card.Content className="card-content">
+        <div onClick={() => recipeDetails(id)} className="card-header">
+          {title}
+        </div>
+        <Card.Meta className="card-meta">
+          <span className="date">id: {id}</span>
+          {user &&
+            (router.pathname === "/bycuisine" ||
+            router.pathname === "/bydish" ? (
+              <SaveButton id={id} title={title} img={img} />
+            ) : (
+              <RemoveButton id={_id} />
+            ))}
+        </Card.Meta>
+      </Card.Content>
+    </motion.div>
   );
 };
 
